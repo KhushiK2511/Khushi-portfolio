8e07f34aaaf48e30ebc3c1efb7a392b1e9d335fa
KhushiK2511 <khushikaradigudd@gmail.com>
2025-11-16 11:05:18 +0530

    Add resume button click handler to open PDF

---
+++ script.js
@@ -0,0 +1,5 @@
+// Resume button click handler
+document.getElementById("resumeBtn").addEventListener("click", function() {
+  // Opens resume.pdf in a new tab
+  window.open("resume.pdf", "_blank");
+});
