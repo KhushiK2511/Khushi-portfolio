60bfaad670c780d87ea4849018f792a12d620854
KhushiK2511 <khushikaradigudd@gmail.com>
2025-11-16 11:13:39 +0530

    Remove comment for resume button click handler
    Removed comment about opening resume.pdf.

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // Resume button click handler
 document.getElementById("resumeBtn").addEventListener("click", function() {
-  // Opens resume.pdf in a new tab
   window.open("resume.pdf", "_blank");
 });
